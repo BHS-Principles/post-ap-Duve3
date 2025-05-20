2c107b1825cd8de2b30e07a910b76c625f4d6987
Dhruv Hooda <Duv3tabest@gmail.com>
2025-05-20 17:41:38 +0000

    why is the web ui not update

--- js.js
+++ js.js
@@ -5,14 +5,16 @@ var coolThing = [
     "amin shun hil"
 ];
 
-alert("please read ts poem");
+// alert("please read ts poem");
 
-setTimeout(() => {
-    alert("Have you read ts poem yet? otherwise you pmo trice 😡")
-}, 1500)
+// setTimeout(() => {
+//     alert("Have you read ts poem yet? otherwise you pmo trice 😡")
+// }, 1500)
 
 
 var button = document.getElementById("amin");
 
 for (var i = 0; i < coolThing.length; i++) {
+    var b2 = button();
+    document.append(b2);
 }
