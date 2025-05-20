143923fb582298304bd494ddce6f80657aaa975f
Dhruv Hooda <Duv3tabest@gmail.com>
2025-05-20 17:25:08 +0000

    new

---
+++ js.js
@@ -0,0 +1,19 @@
+var coolThing = [
+    "what it means",
+    "austin texaws",
+    "austin hillas",
+    "amin shun hil"
+];
+
+alert("please read ts poem");
+
+setTimeout(() => {
+    alert("Have you read ts poem yet? otherwise you pmo trice 😡")
+}, 1500)
+
+
+var button = document.getElementById("amin");
+
+for (var i = 0; i < coolThing.length; i++) {
+    var newButton =  
+}
