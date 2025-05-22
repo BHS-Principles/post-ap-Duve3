a710d37fbb39eaa751c7f7d79a79b856b72da31b
Dhruv Hooda <Duv3tabest@gmail.com>
2025-05-22 17:47:29 +0000

    thanks for useless boiler plate!

--- js.js
+++ js.js
@@ -5,16 +5,20 @@ var coolThing = [
     "amin shun hil"
 ];
 
-// alert("please read ts poem");
-
-// setTimeout(() => {
-//     alert("Have you read ts poem yet? otherwise you pmo trice 😡")
-// }, 1500)
-
-
 var button = document.getElementById("amin");
+
+function Funny(event) {
+    alert("ouch ouch my name is aminnn");
+    console.log(event);
+}
+
+button.addEventListener("click", Funny);
 
 for (var i = 0; i < coolThing.length; i++) {
     var b2 = button.cloneNode(true);
+
+    b2.innerHTML = coolThing[i];
     document.body.append(b2);
+
+    b2.addEventListener("click", Funny);
 }
